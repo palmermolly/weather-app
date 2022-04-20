573f01798b911298f5e87e6f702008d5d3fdecc6
palmermolly <92853563+palmermolly@users.noreply.github.com>
2022-04-20 14:44:23 +0100

    Files transferred from Code Sandbox

---
+++ src/index.js
@@ -0,0 +1,69 @@
+import "./styles.css";
+
+function displayWeatherConditions(response) {
+  console.log(response);
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTMl = response.data.main.humidity;
+  document.querySelector("#wind").innerHTMl = Math.round(
+    response.data.wind.speed
+  );
+}
+
+function search(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-input").value;
+  let units = "metric";
+  let apiKey = "9a1308ed0f5a60a1ad33ac8ed1ebca18";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayWeatherConditions);
+}
+
+let searchInput = document.querySelector("#search-form");
+searchInput.addEventListener("submit", search);
+
+let currentTime = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[currentTime.getDay()];
+let hours = currentTime.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = currentTime.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+let p = document.querySelector("p");
+p.innerHTML = `${day} ${hours}:${minutes}`;
+
+function convertToF(event) {
+  event.preventDefault();
+  let fConversion = document.querySelector("#temperature");
+  let temperature = fConversion.innerHTML;
+  fConversion.innerHTML = Math.round((temperature * 9) / 5 + 32);
+}
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", convertToF);
+
+function convertToC(event) {
+  event.preventDefault();
+  let cConversion = document.querySelector("#temperature");
+  let temperature = cConversion.innerHTML;
+  cConversion.innerHTML = Math.round(((temperature - 32) * 5) / 9);
+}
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", convertToC);
